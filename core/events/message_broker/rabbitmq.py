85f2defb3138894ff11d888ede8fc3e623831e84
DogTag <dogtag@protonmail.com>
2018-02-10 17:15:06 +0530

    simple rmq utils

--- core/events/message_broker/rabbitmq.py
+++ core/events/message_broker/rabbitmq.py
@@ -27,88 +27,64 @@ __all__ = [
 ]
 
 
-class RmqRpc(object):
-    def __init__(self):
-        self.create_connection()
-        self.create_channel()
+class SimpleRabbitMQ(object):
+    def __init__(self, **mq_details):
 
-    def create_connection(self):
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='localhost')
+            pika.ConnectionParameters(
+                **mq_details
+            )
         )
 
-    def create_channel(self):
         self.channel = self.connection.channel()
 
-    def consume(self):
-        self.channel.basic_consume(
-            self.on_response, no_ack=True, queue=self.callback_queue
+    def publish(self, queue, payload):
+
+        self.channel.queue_declare(queue=queue)
+
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=queue,
+            body=payload
         )
 
-    def declare_callback(self):
-        self.callback_queue = self.channel.queue_declare(exclusive=True).method.queue
+        self.close_conn()
 
-
-class RpcServer(RmqRpc):
-    def __init__(self):
-        super(self.__class__, self).__init__()
-
-    def on_request(self, channel, method, props, body):
-
-        response = "Processed Event: {} Successfully".format(body)
-        print 'Received Event {}'.format(body)
-
-        if getattr(props, 'reply_to', None) and props.reply_to:
-            channel.basic_publish(
-                exchange='',
-                routing_key=props.reply_to,
-                properties=pika.BasicProperties(correlation_id=props.correlation_id),
-                body=str(response)
-            )
-
-            channel.basic_ack(delivery_tag=method.delivery_tag)
-
-    def __call__(self):
-        self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(self.on_request, queue='rpc_queue')
+    def consume(self, queue, callback):
+        self.channel.basic_consume(
+            callback, queue=queue, no_ack=True
+        )
 
         self.channel.start_consuming()
 
-class RpcClient(RmqRpc):
-    def __init__(self):
-        super(self.__class__, self).__init__()
-        self.declare_callback()
-        self.consume()
+    def close_conn(self):
+        self.connection.close()
 
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
 
-    def __call__(self, payload):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(exchange='',
-                                   routing_key='rpc_queue',
-                                   properties=pika.BasicProperties(
-                                         reply_to = self.callback_queue,
-                                         correlation_id = self.corr_id,
-                                         ),
-                                   body=str(payload))
-        while self.response is None:
-            self.connection.process_data_events()
-        return self.response
+class SimpleServer(SimpleRabbitMQ):
+    def __init__(self, **mq_details):
+        super(self.__class__, self).__init__(**mq_details)
+
+    def on_response(self, ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+    def listen(self, queue):
+        self.consume(queue, self.on_response)
+
+
+class SimpleProducer(SimpleRabbitMQ):
+    def __init__(self, **mq_details):
+        super(self.__class__, self).__init__(**mq_details)
+
+    def emit(self, queue, payload):
+        self.publish(queue, payload)
+
 
 def event_emittor():
-    emittor = RpcClient()
-
-    while True:
-        import time
-        time.sleep(0.2)
-        _id = str(uuid.uuid4())
-        print '>>> Emiting', _id
-        print emittor('{}'.format(_id))
+    for i in range(5):
+        emitter = SimpleProducer(host='localhost', port=5672)
+        emitter.emit('hello', 'PowerUp')
 
 def event_listener():
-    listener = RpcServer()
-    listener()
-
+    listener = SimpleServer(host='localhost', port=5672)
+    listener.listen('hello')
